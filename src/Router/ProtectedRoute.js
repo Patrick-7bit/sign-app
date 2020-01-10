3d960bf1e9fa58b0e16048d044200dd7fc141e35
Patrick-7bit <dumeixpatrick@hotmail.com>
2020-01-10 15:43:58 +0100

    ajoutfichier.env

--- src/Router/ProtectedRoute.js
+++ src/Router/ProtectedRoute.js
@@ -3,6 +3,7 @@ import { Route, Redirect } from "react-router-dom";
 import { useAuth } from "../Auth";
 
 /*we create a protected route to check if the user is authenticated by a token to get access to the private area. With the hook useAuth we retrieve the token. If the token is missing the user is redirected to the login page*/
+
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const { authTokens } = useAuth();
 
